3cd108c9155822fc6adfe4c0da5b4a330e5ea6c8
AlexandreSaker <saker.alexandre@gmail.com>
2020-12-23 11:10:22 +0100

    initial commit

---
+++ apps/articles-service/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'articles-service',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/articles-service',
+};
